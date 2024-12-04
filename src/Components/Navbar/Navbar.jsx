a705344cc9b91027d7cce88090469784a169b8b8
Vikas Dangi <mvikasdangi@gmail.com>
2024-12-05 00:17:26 +0530

    feat: made navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,15 +6,13 @@ const Navbar = () => {
   const [showMenu, setShowMenu] = useState(true);
 
   return (
-    <nav
-      className={`${
-        menu ? "block" : "hidden"
-      } flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20`}
-    >
+    <nav className="flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20">
       <span className="text-xl font-bold tracking-wide">Portfolio</span>
       <ul
-        className="mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30
-       md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6"
+        className={`${
+          menu ? "block" : "hidden"
+        } mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30
+       md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
       >
         <a href="#about">
           <li className="text-md transition-all duration-300 p-1 md:p-0">
