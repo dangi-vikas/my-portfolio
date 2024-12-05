fff566bf25dc932bcfaa0430321e3fc29e00df6c
Vikas Dangi <mvikasdangi@gmail.com>
2024-12-06 00:34:12 +0530

    feat: added projects section

---
+++ src/Components/Projects/Projects.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const Projects = () => {
+  return (
+    <div id="Projects" className="p-10 md:p-24 text-white ">
+      <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
+      <div className="py-12 px-8 flex flex-wrap gap-5">
+        <ProjectCard
+          title="NexGen"
+          main="Pharmacy dispensing application"
+        />
+        <ProjectCard
+          title="Star App"
+          main="Shift allowance calculator application"
+        />
+        <ProjectCard
+          title="Reddit Clone"
+          main="Clone application for Reddit"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
