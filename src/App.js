5da569f5094d37795ec3add0f0be251ad2dbb14d
Vikas Dangi <mvikasdangi@gmail.com>
2024-12-05 23:31:13 +0530

    feat: added home panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
-      <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
