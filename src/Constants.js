dcd154b0ff2e5ddc43663e231c500084a3e1e512
Vikas Dangi <mvikasdangi@gmail.com>
2024-12-08 22:18:15 +0530

    refactor: updated the constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,11 +1,34 @@
 var HOME_DESC =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi voluptates atque ipsum. Temporibus nostrum quam tempore fugit";
-var JAVA_EXP =
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet fugit provident illo aspernatur deserunt eum, dolorum neque incidunt cumque optio dolores, explicabo tempora minima ullam";
-var REACT_EXP =
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet fugit provident illo aspernatur deserunt eum, dolorum neque incidunt cumque optio dolores, explicabo tempora minima ullam";
-var TCS_EXP = ["fdf", "fsdf", "sdfdsf", "FSDfsd"];
-var INCEDO_EXP = ["fdf", "fsdf", "sdfdsf", "FSDfsd"];
+  "Results-driven Software Engineer with 2+ years of experience in developing scalable, high-performance applications using React and"
+ + " Spring frameworks. Proven expertise in reducing load times by 16% and increasing sales by 10% through efficient implementation of"
+ + " the microservice architecture. Having experience solving complex problems and optimizing systems in fast-paced, product-driven"
+ + " environments. Passionate about delivering quality code and collaborating in Agile teams to build innovative product";
+
+ var JAVA_EXP =
+  "Skilled Java Developer with over 2 years of experience in designing and implementing scalable, "
+  + "high-performance applications. Expertise includes developing RESTful APIs, leveraging microservices"
+  + " architecture, and optimizing systems for efficiency. Proficient in Java, Spring frameworks (Spring Boot, Spring MVC, Spring Security), and tools like JUnit, Mockito, and Azure DevOps. Experienced in building end-to-end solutions, collaborating in Agile teams, and ensuring
+  + " seamless integrations across front-end and back-end systems";
+
+var REACT_EXP = 
+"Proficient in building intuitive and scalable user interfaces using React, with experience in React"
+ + " Context API and Hooks to enhance functionality and user experience. Delivered 50+ features for a "
+ + "high-traffic pharmacy platform, achieving a 10% sales increase and reducing load time by 16%. "
+ + "Skilled in integrating front-end applications with back-end APIs and troubleshooting UI-related "
+ + "issues to optimize performance and reliability.";
+
+var TCS_EXP = ["Developed scalable RESTful APIs for a high-traffic pharmacy platform using Spring Boot, ensuring reliable performance across"
+ +"thousands of daily users. Delivered 50+ features, with 200+ comprehensive tests, improving functionality and user experience"
+ , " Implemented microservices architecture using Spring MVC, Spring Data, and Spring Security, which boosted sales by 10% and"
+ + "reduced application load times by 15%."
+ , "Demonstrated strong problem-solving skills in Troubleshooting complex issues within the application, identifying root causes and"
+ + "implementing effective solutions increasing the speed by 7%."];
+
+var INCEDO_EXP = [" Developed efficient user interfaces using the GWT framework of Java,ensuring optimal performance and user experience for web"
+ + "applications."
+ , "Mastered Core Java,Spring Framework, and ReactJS during arigorous internship, contributing to full-stack development"
+ + "projects."];
+
 var EMAIL = "mvikasdangi@gmail.com";
 var LINKED_IN = "linkedin.com/vikasdangi";
 var LINKED__LINK = "https://www.linkedin.com/in/vikasdangi";
